afe47dc1c105fff87d10fabb239d242d63a03e94
Doston <dostonlv@icloud.com>
2024-01-26 22:09:12 +0500

    getting start rust

---
+++ basic-formatting/formatting.rs
@@ -0,0 +1,37 @@
+// Single Placeholder
+// fn main() {
+//     println!("Number: {}", 1);
+// }
+
+//Multiple Placeholders
+// fn main() {
+//     println!("{} is a {} company", "Educative", "Software");
+// }
+
+//Positional Arguments
+// fn main() {
+//     println!("Enhance your coding skills from {0} courses.  {0} courses are very {1}", "Educative", "interactive");
+// }
+
+// Named Arguments
+// fn main() {
+//     println!("{company} provides {kind} courses", company = "Educative", kind = "interactive");
+// }
+
+// Placeholder Traits
+// {:b} -> binary, {:x} -> hexadecimal, {:o} -> octal
+// fn main() {
+//     println!("Number : 10 \nBinary:{:b} Hexadecimal:{:x} Octal:{:o}", 10, 10, 10);
+// }
+
+// Basic Math
+// fn main() {
+//     println!("{} + {} = {}",10, 10, 10 + 10);
+// }
+
+// Placeholder for a Debug Trait
+// {:?} -> Debug Trait
+// fn main() {
+//     println!("{:?}", ("This is a Rust Course", 101));
+// }
+
---
+++ printing-styles/print.rs
@@ -0,0 +1,28 @@
+// print!() -> prints to the screen (no newline)
+
+// fn main() {
+//     print!("Rust Programming");
+//     print!(" Course"); // Rust Programming Course 
+// }
+
+// The println!() macro appends a new line at the end of the string.
+// fn main() {
+//     println!("Rust Programming");
+//     println!("Course");
+// }
+//  output: Rust Programming
+//  Course
+
+// The eprint!() macro displays the output as an error.
+// fn main() {
+//     eprint!("Rust Programming");
+//     eprint!(" Course"); / /Rust Programming Course 
+// }
+
+// The eprint!() macro displays the output as an error and appends a new line(\n) at the end of it.
+// fn main() {
+//     eprintln!("Rust Programming");
+//     eprintln!("Course");
+// }
+// output: Rust Programming
+// Course
